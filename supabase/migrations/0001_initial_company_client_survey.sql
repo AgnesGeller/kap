8ce7af0d54d5c698321698affddeaef8c8904630
AgnesGeller <agnesgeller@cyberanalys.com>
2026-06-09 14:28:38 +0200

    Supabase search path biztonsagi javitas

--- supabase/migrations/0001_initial_company_client_survey.sql
+++ supabase/migrations/0001_initial_company_client_survey.sql
@@ -25,6 +25,7 @@ create type public.survey_source as enum (
 create or replace function public.set_updated_at()
 returns trigger
 language plpgsql
+set search_path = public
 as $$
 begin
   new.updated_at = timezone('utc', now());
